296bf6254948660faa5ade28fbe28d86e9fb860c
agustinmassa <agustinmassa8@gmail.com>
2024-02-24 08:42:57 -0300

    Añadido proyecto

---
+++ webempresa/services/admin.py
@@ -0,0 +1,8 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Service
+
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Service, ServiceAdmin)
---
+++ webempresa/services/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+
+from django.shortcuts import render
+from .models import Service
+
+def services(request):
+    services = Service.objects.all()
+    return render(request, "services/services.html", {'services':services})
